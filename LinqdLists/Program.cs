23875dddb428c12c8a48dc2ebfa7112eef3c0916
Andy Million <amillion3@gmail.com>
2018-09-19 18:51:16 -0500

    Add project files.

---
+++ LinqdLists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqdLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
