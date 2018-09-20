0a62cd704e4324354792a39206dfc6f1c706d68c
Andy Million <amillion3@gmail.com>
2018-09-19 19:59:51 -0500

    WIP

--- LinqdLists/Program.cs
+++ LinqdLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LinqdLists
 {
@@ -6,7 +7,20 @@ namespace LinqdLists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>
+            {
+                "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"
+            };
+            char matchingLetter = 'l';
+            IEnumerable<string> query1 = from fruit in fruits
+                                         where fruit.StartsWith < matchingLetter >
+                                         select fruit;
+
+            foreach (var abc in query1)
+            {
+                Console.WriteLine(abc);
+            }
         }
     }
 }
