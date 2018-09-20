c1795440960109fbb3418083102230d9f0b4eeae
Andy Million <amillion3@gmail.com>
2018-09-19 21:12:51 -0500

    finished part 3

--- LinqdLists/Program.cs
+++ LinqdLists/Program.cs
@@ -34,9 +34,55 @@ namespace LinqdLists
                                    select number;
 
             Console.WriteLine("4 and 6 multiples:");
-            foreach (int f in fourSixMultiples) { Console.WriteLine(f); };
+            foreach (int f in fourSixMultiples) {
+                Console.WriteLine(f);
+            };
             Console.ReadLine();
             Console.WriteLine("");
+
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+                {
+                    "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                    "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                    "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                    "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                    "Francisco", "Tre"
+                };
+
+            var descend = from name in names
+                          orderby name descending
+                          select name;
+
+            Console.WriteLine("Descending names:");
+            foreach (string f in descend)
+            {
+                Console.WriteLine(f);
+            };
+            Console.ReadLine();
+            Console.WriteLine("");
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers2 = new List<int>()
+            {
+                15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+            };
+
+            var sortEm = from number2 in numbers2
+                         orderby number2 ascending
+                         select number2;
+
+            List<int> sortedNumbers = new List<int>();
+
+            Console.WriteLine("Ascending numbers:");
+            foreach (var f in sortEm)
+            {
+                sortedNumbers.Add(f);
+                Console.WriteLine(f);
+            }
+            Console.ReadLine();
+            Console.WriteLine("");
+
         }
     }
 }
