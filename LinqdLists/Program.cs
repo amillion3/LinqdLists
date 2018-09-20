1f12c37af46b54f16e4f4273ce0030c86885e027
Andy Million <amillion3@gmail.com>
2018-09-19 20:47:49 -0500

    part 2 complete

--- LinqdLists/Program.cs
+++ LinqdLists/Program.cs
@@ -15,12 +15,28 @@ namespace LinqdLists
                           where fruit.StartsWith("L") == true
                           select fruit;
 
-            foreach (var f in matches)
+            Console.WriteLine("Fruits that begin with an L character: ");
+            foreach (string f in matches)
             {
                 Console.WriteLine(f);
             };
             Console.ReadLine();
+            Console.WriteLine("");
 
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+                {
+                    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+                };
+
+            var fourSixMultiples = from number in numbers
+                                   where number % 4 == 0 || number % 6 == 0
+                                   select number;
+
+            Console.WriteLine("4 and 6 multiples:");
+            foreach (int f in fourSixMultiples) { Console.WriteLine(f); };
+            Console.ReadLine();
+            Console.WriteLine("");
         }
     }
 }
