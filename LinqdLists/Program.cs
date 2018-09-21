425c4fd84111face1eaf26bf7f6278de20d1da06
Andy Million <amillion3@gmail.com>
2018-09-21 15:14:31 -0500

    WIP

--- LinqdLists/Program.cs
+++ LinqdLists/Program.cs
@@ -6,6 +6,13 @@ namespace LinqdLists
 {
     class Program
     {
+
+        public class Customer
+        {
+            public string Name { get; set; }
+            public double Balance { get; set; }
+            public string Bank { get; set; }
+        }
         static void Main(string[] args)
         {
             // Find the words in the collection that start with the letter 'L'
@@ -34,7 +41,8 @@ namespace LinqdLists
                                    select number;
 
             Console.WriteLine("4 and 6 multiples:");
-            foreach (int f in fourSixMultiples) {
+            foreach (int f in fourSixMultiples)
+            {
                 Console.WriteLine(f);
             };
             Console.ReadLine();
@@ -141,8 +149,49 @@ namespace LinqdLists
             Console.ReadLine();
             Console.WriteLine("");
 
+            // Build a collection of customers who are millionaires
+
+
+            List<Customer> customers = new List<Customer>() {
+            new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+        };
+
+            var richPeople = from c in customers
+                             where c.Balance >= 1000000
+                             select c;
+
+            var results = from r in richPeople
+                          group r.Bank by r.Name into g
+                          select new { Name = g.Key, Bank = g.ToList() };
+
+            foreach (var res in g)
+            {
+                Console.WriteLine(res);
+            }
+            Console.ReadLine();
+            Console.WriteLine("");
+
+            /* 
+                Given the same customer set, display how many millionaires per bank.
+                Ref: https://stackoverflow.com/questions/7325278/group-by-in-linq
+
+                Example Output:
+                WF 2
+                BOA 1
+                FTB 1
+                CITI 1
+            */
 
         }
     }
-}
-;
+
+};
